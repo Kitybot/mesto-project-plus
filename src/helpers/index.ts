0738c5ff58e128750a7b60e3adc6e2beb23801a6
Kitybot <nick34goalie@yandex.ru>
2023-07-11 16:23:59 +0300

    commit: готов к ревью

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,21 +2,33 @@ import { Response } from 'express';
 import mongoose from 'mongoose';
 import CodesHTTPStatus from '../types/codes';
 
-export const handleErrors = (err: any, res: Response) => {
-  if (err instanceof mongoose.Error.CastError || err instanceof mongoose.Error.ValidationError) {
-    res.status(CodesHTTPStatus.BAD_REQUEST).json({
-      message: `Переданы некорректные данные: ${err.message}`,
-    });
-  } else if (err.name === 'MongoServerError') {
-    res.status(CodesHTTPStatus.CONFLICT).json({
-      message: `Повторное использование ${Object.keys(err.keyValue)[0]} - ${Object.values(err.keyValue)[0]}, который должен быть уникальным. Используйте другой ${Object.keys(err.keyValue)[0]}.`,
-    });
+interface MongooseError extends mongoose.Error {
+  keyValue?: Record<string, string>;
+}
+
+export const handleErrors = (err: MongooseError | { statusCode?: number; message: string }, res: Response) => {
+  if (isMongooseError(err)) {
+    if (err instanceof mongoose.Error.CastError || err instanceof mongoose.Error.ValidationError) {
+      res.status(CodesHTTPStatus.BAD_REQUEST).json({
+        message: `Переданы некорректные данные: ${err.message}`,
+      });
+    } else if (err.name === 'MongoServerError') {
+      if (err.keyValue) {
+        res.status(CodesHTTPStatus.CONFLICT).json({
+          message: `Повторное использование ${Object.keys(err.keyValue)[0]} - ${Object.values(err.keyValue)[0]}, который должен быть уникальным. Используйте другой ${Object.keys(err.keyValue)[0]}.`,
+        });
+      }
+    }
   } else if (!err.statusCode) {
     res.status(CodesHTTPStatus.DEFAULT).json({
       message: 'На сервере произошла ошибка',
     });
   } else {
-    res.status(err.statusCode).json(err.message);
+    res.status(err.statusCode).json({ message: err.message });
   }
 };
+
+function isMongooseError(err: any): err is MongooseError {
+  return err instanceof mongoose.Error;
+}
 export default handleErrors;
